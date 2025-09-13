c46db222fa02f0492586e6ea4e5b2ae717b20704
[dyad] <gustavogama099@gmail.com>
2025-09-12 23:15:09 -0300

    [dyad] Corrigindo erro de sintaxe na importação do React - wrote 1 file(s)

--- src/components/service-orders/VisualChecklist.tsx
+++ src/components/service-orders/VisualChecklist.tsx
@@ -1,4 +1,4 @@
-import React, 'react';
+import React from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
