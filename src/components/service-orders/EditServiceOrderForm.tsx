ac1d731432dc5862c95260942a51dcf5d899f104
[dyad] <gustavogama099@gmail.com>
2025-09-13 23:06:58 -0300

    [dyad] Corrigindo importação de `FormDescription`. - wrote 1 file(s)

--- src/components/service-orders/EditServiceOrderForm.tsx
+++ src/components/service-orders/EditServiceOrderForm.tsx
@@ -10,6 +10,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription, // Adicionado FormDescription
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
